a42ce75cbd0f1adf564e8aa9a1f28520e6be1c12
Halehkm <halehmotlagh@gmail.com>
2021-12-02 19:00:33 -0600

    Added bootstrap component

--- src/Definition.js
+++ src/Definition.js
@@ -1,12 +1,13 @@
 import Synonym from "./Synonym";
 import Antonym from "./Antonym";
 import Accordion from "react-bootstrap/Accordion";
+import "./Accordion.scss";
 
 export default function Definition(props) {
   return (
     <Accordion>
       <Accordion.Item eventKey="0">
-        <Accordion.Header>Definition</Accordion.Header>
+        <Accordion.Header className="header">Definition</Accordion.Header>
         <Accordion.Body>
           <p>{props.definition.definition}</p>
         </Accordion.Body>
@@ -19,8 +20,9 @@ export default function Definition(props) {
       </Accordion.Item>
       <Accordion.Item eventKey="2">
         <Accordion.Header>Synonyms</Accordion.Header>
-        <Synonym synonym={props.definition.synonyms} />
-        <Accordion.Body></Accordion.Body>
+        <Accordion.Body>
+          <Synonym synonym={props.definition.synonyms} />
+        </Accordion.Body>
       </Accordion.Item>
       <Accordion.Item eventKey="3">
         <Accordion.Header>Antonyms</Accordion.Header>
--- src/Meaning.js
+++ src/Meaning.js
@@ -2,7 +2,7 @@ import Definition from "./Definition";
 
 export default function Meaning(props) {
   return (
-    <div className="Meaning">
+    <div className="Meaning text-capitalize">
       <h4>{props.meaning.partOfSpeech}</h4>
 
       {props.meaning.definitions.map(function (definition, index) {
