36fe519320166f8156d16ad71c66fe1fe078fd07
Halehkm <halehmotlagh@gmail.com>
2021-12-03 23:23:55 -0600

    Added phonetic audio

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,5 +1,19 @@
+import ReactAudioPlayer from "react-audio-player";
+
 export default function Phonetic(props) {
   console.log(props);
 
-  return <div className="Phonetic">{props.phonetics.text}</div>;
+  return (
+    <>
+      <ReactAudioPlayer
+        className="react-audio-player AudioPlayer"
+        controls=""
+        id=""
+        preload="metadata"
+        src={props.phonetics.audio}
+        title={props.phonetics.audio}
+      />
+      <div className="Phonetic">{props.phonetics.text}</div>
+    </>
+  );
 }
